39fb8adda9bfaa0e9e3af9a16e5ade562bf79376
Saki <sbasken@gmail.com>
2023-03-24 13:38:05 -0700

    set up alembic

---
+++ lib/migrations/versions/b63d847672c0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b63d847672c0
+Revises: 
+Create Date: 2023-03-24 13:35:04.885719
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b63d847672c0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
